c0de29e36e08a22a39fa03c7b38c611794dd7379
ichyo <ichyo00@gmail.com>
2018-01-18 23:43:53 +0900

    Fix new.sh

--- rust/new.sh
+++ rust/new.sh
@@ -13,13 +13,12 @@ fi
 
 ARG=$1
 
-if ! [[ $ARG =~ ^(arc|abc)[0-9]{3}_[abcd]$ ]]; then
+if ! [[ $ARG =~ ^(arc|abc|agc)[0-9]{3}_[abcd]$ ]]; then
     usage
 fi
 
-DOMAIN=$(echo $ARG | cut -d '_' -f 1).contest.atcoder.jp
-URL=http://$DOMAIN/tasks/$ARG
+CONTEST=$(echo $ARG | cut -d '_' -f 1)
+URL=https://beta.atcoder.jp/contests/$CONTEST/tasks/$ARG
 
 cp -n ./template.rs ./src/bin/$ARG.rs 
 curl $URL | tr -d \\r | python3 ./scripts/parse.py ./resource/$ARG
-code ./src/bin/$ARG.rs
