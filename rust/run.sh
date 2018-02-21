cec00efe06dbe5b09e04d40ef3621c8dc7d66482
ichyo <ichyo00@gmail.com>
2018-02-21 09:20:21 +0900

    Fix shell script

--- rust/run.sh
+++ rust/run.sh
@@ -14,7 +14,7 @@ fi
 
 ARG=$1
 
-cargo build --bin $ARG
+cargo +1.15.1 build --bin $ARG
 
 FAILED=0
 function failed {
@@ -22,12 +22,12 @@ function failed {
     echo "...Failed"
 }
 
-for file in $(find ./resource/$ARG/input/ -type f); do
+for file in $(find ./resource/$ARG/input/ -type f | sort); do
     IDX=$(basename $file)
     INPUT="./resource/$ARG/input/$IDX"
     OUTPUT="./resource/$ARG/output/$IDX"
     echo " --- Case $IDX --- "
-    (./target/release/$ARG < $INPUT | diff - $OUTPUT && echo "OK") || failed
+    (./target/debug/$ARG < $INPUT | diff - $OUTPUT && echo "OK") || failed
 done
 
 if [ $FAILED == 0 ]; then
